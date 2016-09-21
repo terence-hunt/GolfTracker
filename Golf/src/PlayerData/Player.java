a1f946d7c6a50192e4b5fb16339a504f1acf054b
Terence Hunt <terence.hunt@hpe.com>
2016-09-21 20:18:55 +0100

    added club object to each player

--- Golf/src/PlayerData/Player.java
+++ Golf/src/PlayerData/Player.java
@@ -1,18 +1,25 @@
 package PlayerData;
 
+import java.util.ArrayList;
 import java.util.Date;
+
+import PlayerData.GolfClubs.Club;
+import PlayerData.GolfClubs.ClubsFactory;
 
 public class Player {
 	String name;
 	Date DOB;
 	float handicap;
+	ArrayList<Club> clubs;
 	
-	public Player(String name, Date DOB, float handicap){
+	public Player(String name, Date DOB, float handicap, ClubsFactory factory){
 		this.name = name;
 		this.DOB = DOB;
 		this.handicap = handicap;
-		
+		clubs = new ArrayList<Club>();
+		factory.getSetOfClubs();
 	}
+	
 	public String getPlayerName(){
 		return name;
 	}
